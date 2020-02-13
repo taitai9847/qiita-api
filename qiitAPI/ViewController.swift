7a962f3ac52902ea8519cc474ff4993d5f80d6cd
ishikawa taiyo <taiyo.ishikawa@ietty.co.jp>
2020-02-13 16:02:28 +0900

    Initial Commit

---
+++ qiitAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiitAPI
+//
+//  Created by ishikawa on 2020/02/13.
+//  Copyright © 2020 ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
